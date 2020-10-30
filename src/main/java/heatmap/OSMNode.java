f45e7d276c3a6bae155198424b73c9cf1b423bf9
TeoMCh <sdi1700197@di.uoa.gr>
2020-10-30 22:45:22 +0200

    miatrupastonnero

---
+++ src/main/java/heatmap/OSMNode.java
@@ -0,0 +1,45 @@
+//package heatmap;
+//
+//import java.util.Map;
+//
+//public class OSMNode {
+//
+//    private String id;
+//
+//    private String lat;
+//
+//    private String lon;
+//
+//    private  Map<String, String> tags;
+//
+//    private String version;
+//
+//    OSMNode( String _id, String _latitude, String _longitude, String _version, Map _tags){
+//        id = _id;
+//        lat = _latitude;
+//        lon = _longitude;
+//        tags = _tags;
+//        version = _version;
+//
+//    }
+//
+//    public String getId() {
+//        return id;
+//    }
+//
+//    public Map<String, String> getTags() {
+//        return tags;
+//    }
+//
+//    public String getLat() {
+//        return lat;
+//    }
+//
+//    public String getLon() {
+//        return lon;
+//    }
+//
+//    public String getVersion() {
+//        return version;
+//    }
+//}

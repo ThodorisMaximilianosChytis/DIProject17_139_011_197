7751cc0669c69d2a6649e26d9ff4da85cfc4a02c
TeoMCh <sdi1700197@di.uoa.gr>
2020-11-03 23:52:54 +0200

    jheatchart

--- src/main/java/heatmap/ImageGrid.java
+++ src/main/java/heatmap/ImageGrid.java
@@ -1,7 +1,7 @@
 package heatmap;
 
-import java.awt.Graphics;
-import java.awt.GridBagLayout;
+import java.awt.*;
+
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.logging.Level;
@@ -15,13 +15,14 @@ import javax.swing.SwingUtilities;
 
 public class ImageGrid extends JLabel {
 
-    private static final int CELLS = 10;
+//    private static final int CELLS = 10;
+//    private static final int
 
     BufferedImage img;
 
     public ImageGrid() {
         try {
-            img = ImageIO.read(getClass().getResource("~/home/thodorismchytis/Documents/PROJECT/DIProject17_139_011_197/Input Data/map.png"));
+            img = ImageIO.read(getClass().getClassLoader().getResource("Map.png"));
             setIcon(new ImageIcon(img));
         } catch (IOException ex) {
             Logger.getLogger(ImageGrid.class.getName()).log(Level.SEVERE, null, ex);
@@ -32,16 +33,26 @@ public class ImageGrid extends JLabel {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         if (img != null) {
-            int cellHeight = (int) (getHeight() / CELLS);
-            int cellWidth = (int) (getWidth() / CELLS);
+//            Image background = Toolkit.getDefaultToolkit().createImage(map)
+//            g.drawImage(img,0,0,null);
+            int cellHeight = (int) (getHeight() / 4.0);
+            int cellWidth = (int) (getWidth() / 10.0);
             for (int y = 0; y < getHeight(); y += cellHeight) {
                 for (int x = 0; x < getWidth(); x += cellWidth) {
+                    g.setColor(Color.BLACK);
                     g.drawRect(x, y, cellWidth, cellHeight);
+                    System.out.println(cellWidth);
+                    System.out.println(cellHeight);
+//                    g.setColor(Color.CYAN);
+//                    g.fillRect(x, y, cellWidth, cellHeight);
 
                 }
             }
         }
     }
+
+
+
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
@@ -49,6 +60,9 @@ public class ImageGrid extends JLabel {
                 JFrame frame = new JFrame();
                 JPanel wrapperPanel = new JPanel(new GridBagLayout());
                 wrapperPanel.add(new ImageGrid());
+//                wrapperPanel.setBackground(new ImageGrid());
+//                wrapperPanel.setOpaque(true);
+//                wrapperPanel.setBackground(new Color(0,0,0,0));
                 frame.add(wrapperPanel);
                 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                 frame.pack();

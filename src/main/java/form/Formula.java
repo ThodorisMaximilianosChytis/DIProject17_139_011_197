ddd78d79005638e557ea433feb8828e907cfe679
PaschalisVals <paschalisvals@gmail.com>
2020-12-30 11:33:44 +0200

    evgala ta sxolia

--- src/main/java/form/Formula.java
+++ src/main/java/form/Formula.java
@@ -1,45 +1,45 @@
-//package form;
-//
-//import java.util.*;
-//
-//public class Formula {
-//
-//    private int R = 6371 * (Math.pow(10, 6));
-//    public double late;
-//    public double longe;
-//
-//    public Formula ( double latstart, double longstart, double angle, double speed ) {
-//
-//        double d = speed/R;
+package form;
+
+import java.util.*;
+
+public class Formula {
+
+    private int R = 6371 * (Math.pow(10, 6));
+    public double late;
+    public double longe;
+
+    public Formula ( double latstart, double longstart, double angle, double speed ) {
+
+        double d = speed/R;
+        double latstartradian = Math.toRadians(latstart);
+        double longstartradian = Math.toRadians(longstart);
+
+        double latendradian = Math.asin(Math.sin(latstartradian) * Math.cos(d) + Math.cos(latstartradian) * Math.sin(d) * Math.cos(angle));
+        double longendradian = longstartradian + Math.atan2(Math.sin(angle) * Math.sin(d) * Math.cos(latstartradian), Math.cos(d) - Math.sin(latstartradian) * Math.sin(latendradian));
+
+        this.late = Math.toDegrees(latendradian);
+        this.longe = Math.toDegrees(longendradian);
+
+
+    }
+
+    public double getlatend() {
+        return late;
+    }
+
+    public double getlongend() {
+        return longe;
+    }
+
+
+//    public Formula formula ( double latstart, double longstart, double angle, double speed ) {
+//        long double d = speed/R;
 //        double latstartradian = Math.toRadians(latstart);
 //        double longstartradian = Math.toRadians(longstart);
 //
 //        double latendradian = Math.asin(Math.sin(latstartradian) * Math.cos(d) + Math.cos(latstartradian) * Math.sin(d) * Math.cos(angle));
 //        double longendradian = longstartradian + Math.atan2(Math.sin(angle) * Math.sin(d) * Math.cos(latstartradian), Math.cos(d) - Math.sin(latstartradian) * Math.sin(latendradian));
 //
-//        this.late = Math.toDegrees(latendradian);
-//        this.longe = Math.toDegrees(longendradian);
-//
-//
+//        return new Formula(Math.toDegrees(latendradian),Math.toDegrees(longendradian));
 //    }
-//
-//    public double getlatend() {
-//        return late;
-//    }
-//
-//    public double getlongend() {
-//        return longe;
-//    }
-//
-//
-////    public Formula formula ( double latstart, double longstart, double angle, double speed ) {
-////        long double d = speed/R;
-////        double latstartradian = Math.toRadians(latstart);
-////        double longstartradian = Math.toRadians(longstart);
-////
-////        double latendradian = Math.asin(Math.sin(latstartradian) * Math.cos(d) + Math.cos(latstartradian) * Math.sin(d) * Math.cos(angle));
-////        double longendradian = longstartradian + Math.atan2(Math.sin(angle) * Math.sin(d) * Math.cos(latstartradian), Math.cos(d) - Math.sin(latstartradian) * Math.sin(latendradian));
-////
-////        return new Formula(Math.toDegrees(latendradian),Math.toDegrees(longendradian));
-////    }
-//}
+}

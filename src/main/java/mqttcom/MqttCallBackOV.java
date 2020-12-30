db6d2123954f35714a751def548cb509366550cd
PaschalisVals <paschalisvals@gmail.com>
2020-12-30 14:42:49 +0200

    split the string

--- src/main/java/mqttcom/MqttCallBackOV.java
+++ src/main/java/mqttcom/MqttCallBackOV.java
@@ -11,7 +11,23 @@ public class MqttCallBackOV implements MqttCallback {
     }
 
     public void messageArrived(String s, MqttMessage mqttMessage) throws Exception {
-        System.out.println("Message received:\t"+ new String(mqttMessage.getPayload()) );
+        String str = new String(mqttMessage.getPayload());
+        //System.out.println("Message received:\t"+ new String(mqttMessage.getPayload()) );
+        System.out.println("Message received:\t"+ str );
+
+        String[] arrOfStr = str.split("|", 8);
+        //0 time, 1 id, 2 lat,3 long,4 angle,5 speed,6 RSSI,7 Throughput
+
+        double time = Double.parseDouble(arrOfStr[0]);
+        int id = Integer.parseInt(arrOfStr[1]);
+        double lat = Double.parseDouble(arrOfStr[2]);
+        double lon = Double.parseDouble(arrOfStr[3]);
+        double angle = Double.parseDouble(arrOfStr[4]);
+        double speed = Double.parseDouble(arrOfStr[5]);
+        double RSSI = Double.parseDouble(arrOfStr[6]);
+        double Throughput = Double.parseDouble(arrOfStr[7]);
+
+        Formula f = new Formula(lat,lon,angle,speed);
     }
 
     public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
